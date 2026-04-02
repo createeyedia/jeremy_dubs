cbb381389f284bd9cd78fd580982e156b66f9482
createeyedia <88696984+createeyedia@users.noreply.github.com>
2026-04-02 13:06:51 -0700

    update site URL to cascadecreatives.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+
 export default defineConfig({
-  site: 'https://jeremydubs.com',
+  site: 'https://cascadecreatives.com',
   integrations: [sitemap()],
   output: 'static',
 });
+
